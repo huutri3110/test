61a268a8c80c69f9c674f81af6c990d0ef3d06a9
Paimom <trandinhvan0294@gmail.com>
2023-02-24 15:21:40 +0700

    first

---
+++ main.js
@@ -0,0 +1,16 @@
+dycalendar.draw ({
+  target: '#dycalendar',
+  type: 'month',
+  // month: 0,
+  // year: 2023,
+  // date: 10,
+  // highlighttargetdate: true,
+  highlighttoday: true,
+  prevnextbutton: 'show',
+})
+
+// var d = document.querySelector ('#dycalendar table');
+
+// d.addEventListener ('click', (e) => {
+//   console.log (e.target.innerHTML)
+// })
